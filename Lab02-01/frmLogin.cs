512c59f713f9c98885a883db45c9c1d601df6625
Franco16021998 <franco.starke@tecsup.edu.pe>
2021-09-07 19:07:52 -0500

    Login con sql

--- Lab02-01/frmLogin.cs
+++ Lab02-01/frmLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Lab02_01
 {
@@ -19,22 +20,26 @@ namespace Lab02_01
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
-            var Usuario = "Franco";
-            var Password = "123456";
-            if (txtUsuario.Text == Usuario && txtPassword.Text == Password)
+            SqlConnection cn = new SqlConnection("Server=FRANCO\\SQLEXPRESS2017; Database=db_lab03; Integrated Security=true");
+            cn.Open();
+            SqlCommand cm = new SqlCommand("SELECT usuario_nombre,usuario_password FROM tbl_usuario WHERE usuario_nombre='" + txtUsuario.Text + "' and usuario_password= '" + txtPassword.Text + "'", cn);
+            SqlDataReader dr = cm.ExecuteReader();
+            if (dr.Read())
             {
-                PrincipalMDI principal = new PrincipalMDI();
-                principal.Show();
+                PrincipalMDI form1 = new PrincipalMDI();
+                form1.Show();
                 this.Hide();
             }
-
             else
             {
-                MessageBox.Show("Cuenta incorrecta", "Error",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error de autentificación");
             }
-            
 
+        }
+
+        private void login()
+        {
+            SqlConnection
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
