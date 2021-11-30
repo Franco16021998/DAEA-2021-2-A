b9fa94dd7879c0a3bb90e95e97ada1e40144b9b4
Franco16021998 <franco.starke@tecsup.edu.pe>
2021-11-30 11:24:20 -0500

    Examen4

---
+++ Biblioteca/Controllers/Autor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biblioteca.Controllers
+{
+    public class Autor
+    {
+        public int IdAutor { get; set; }
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+
+    }
+}
